3f9bd61a46b96ea3f68a35924337ef2f3017be07
Geonhwi Park <geonhwipark715@gmail.com>
2024-05-05 19:48:25 +0900

    feat: page.js

--- controllers/page.js
+++ controllers/page.js
@@ -1,3 +1,9 @@
-exports.renderProfile = (req, res, next) => {};
-exports.renderJoin = (req, res, next) => {};
-exports.renderMain = (req, res, next) => {};
+exports.renderProfile = (req, res, next) => {
+    res.render('profile', { title: '내 정보 - NodeBird' });
+};
+exports.renderJoin = (req, res, next) => {
+    res.render('join', { title: '회원가입 - NodeBird' });
+};
+exports.renderMain = (req, res, next) => {
+    res.render('main', { title: 'NodeBird' });
+};
