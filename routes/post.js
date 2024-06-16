cfc66b7b48cf6a3a1457a689268d229f45bb98ca
Geonhwi Park <geonhwipark715@gmail.com>
2024-06-17 01:36:06 +0900

    게시글 수정 관련

--- routes/post.js
+++ routes/post.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 
 const { afterUploadImage, uploadPost } = require('../controllers/post');
 const { isLoggedIn } = require('../middlewares');
+const { Post } = require('../models');
 
 const router = express.Router();
 
@@ -34,5 +35,22 @@ router.post('/img', isLoggedIn, upload.single('img'), afterUploadImage);
 // POST /post
 const upload2 = multer();
 router.post('/', isLoggedIn, upload2.none(), uploadPost);
+
+// GET /post/:id/delete
+router.get('/:id/delete', isLoggedIn, async (req, res, next) => {
+    try {
+        const post = await Post.findOne({ where: { id: req.params.id }});
+        if (post) {
+            // 게시글의 작성자와 현재 로그인한 사용자가 같은지 확인하는 로직 제거
+            await post.destroy();
+            res.status(204).send('게시글이 성공적으로 삭제되었습니다.');
+        } else {
+            res.status(404).send('해당 ID의 게시글이 존재하지 않습니다.');
+        }
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
 
 module.exports = router;
